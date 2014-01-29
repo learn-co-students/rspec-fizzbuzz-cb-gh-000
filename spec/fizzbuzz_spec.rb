628a08ea8ad5dabf670692b8642e76f798f67a68
Logan Hasson <logan.hasson@gmail.com>
2014-01-29 15:02:51 -0500

    Edits to README and update spec to fix incorrect expectation

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,7 +6,7 @@ describe "fizzbuzz" do
 
     expect(fizz_3).to eq("Fizz")
   end
-  it 'returns "Buzz" when the number is divisible by 3' do
+  it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
 
     expect(fizz_5).to eq("Buzz")
