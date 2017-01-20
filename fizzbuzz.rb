3b1f5206a1302998f0154debafdb57166e90d10b
David Vasquez <clawthedruid@gmail.com>
2017-01-20 19:27:30 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 #fizzbuzz code
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  elsif num % 3 == 0
+  elsif int % 3 == 0
     return "Fizz"
-  elsif num % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
   else
     return nil
