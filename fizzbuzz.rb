b9af551968f2d7995ee39c040c11c4bce11fd935
Artem Belov <artbelov@live.ru>
2017-01-29 14:23:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(any)
   if any % 3 == 0 && any % 5 == 0
-      val="FizzBuzz"
-    elsif any % 3 == 0
-        val="Fizz"
-      elsif any % 5 == 0
-          val="Buzz"
+    "FizzBuzz"
+  elsif any % 3 == 0
+    "Fizz"
+  elsif any % 5 == 0
+    "Buzz"
   end 
-end   
+end
