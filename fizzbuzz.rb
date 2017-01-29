8607361b94acfc04a2693d127aecee55382d5906
Abdurrahman ibnHamdan <ibnhamdan@msn.com>
2017-01-29 20:25:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,12 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 != 0
     "Fizz"
+  elsif int % 5 == 0 && int % 3 != 0
+    "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else
+    nil
   end
 end
 
