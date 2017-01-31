69b5da89352eea6ac5f03adb30aa452d06523bf0
Sen Deng <dandirion@hotmail.com>
2017-01-31 15:10:16 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else (int % 3 != 0) && (int % 5 != 0)
+    nil
+  end
+end
