2caabeb63165b257c86b2cdb02d301a242088ca9
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2017-02-06 19:40:06 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,28 @@
+def fizzbuzz(int)
+
+if int <= 5
+  if int %  3 == 0
+
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+
+else
+
+  if int % 5 == 0 && int % 3 == 0
+
+  return "FizzBuzz"
+end 
+end
+
+
+
+
+
+end
+
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
