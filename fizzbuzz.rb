7e9bbf1b5c64871cff41812b3f76b0b1229cfdc7
Katerina Patticha <kate@kpatticha.com>
2017-02-06 21:17:12 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(number)
+
+  if number%3 == 0 && number%5 == 0
+    return "FizzBuzz"
+  elsif number%3 == 0
+    return "Fizz"
+  elsif number%5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+
+end
+fizzbuzz(3)
+fizzbuzz(9)
+fizzbuzz(15)
+fizzbuzz(10)
