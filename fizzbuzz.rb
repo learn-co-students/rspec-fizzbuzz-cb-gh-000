069edfc4bfa46b34af52bf2775c2f97446608067
Francisco Martinez <fmartinez92@gmail.com>
2017-02-20 20:51:39 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(num)
+  if (num % 3 == 0 && num % 5 == 0)
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif (num % 3 == 0 && num % 5 == 0)
+    "FizzBuzz"
+  else
+    nil
+  end
+end
