e2bda859a159cb84406eca67157aea08f0589a8e
Corrie Rands <corrie.rands@hotmail.com>
2017-02-25 04:00:08 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
-    "FizzBuzz" # Go fizz
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz" 
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
