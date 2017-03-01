4abd321fc3843c561fec6bb62f4b6c6efeeaa204
ahmad sulasi <ahmad.luthfi@student.upi.edu>
2017-03-01 15:04:02 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(int)
+  if int %3 == 0 && int %5==0
+    "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif int % 5 == 0
+    "Buzz"
+  end
+end
+ 
