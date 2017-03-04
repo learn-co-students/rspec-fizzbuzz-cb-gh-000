788ad3308dfa8b080bb3745c0a3d9283385ade2c
Marlos Pomin <marlospomin@gmail.com>
2017-03-04 17:09:44 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(number)
+  if (number % 3 == 0 && number % 5 == 0)
+    "FizzBuzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  else
+    nil
+  end
+end
