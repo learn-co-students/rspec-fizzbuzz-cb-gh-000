bfae0243df81023dfb02e0d9b3c91a76523a828b
Anshul Chauhan <anshulchauhan007@gmail.com>
2017-04-03 16:39:28 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  end
+  return nil
+end
