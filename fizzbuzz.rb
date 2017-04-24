6f946d5cf008ee4c95f77ab99e554ef0ef94c376
Michael Nguyen <mikemhn@gmail.com>
2017-04-24 07:27:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 15 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
   else
-    "FizzBuzz"
+    nil
+end
 end
