b78d36f7a83bd9e9225d8eed413a68ae0aed994f
Michael Aworghene <zonalmeet@gmail.com>
2017-05-07 10:41:21 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+int = 15
+
+def fizzbuzz(int)
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  else
+    puts "Not divisible!"
+
+  end
+
+end
