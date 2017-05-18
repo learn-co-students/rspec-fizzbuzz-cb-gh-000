513bb65724f0207b85f393206560b29fa18888b4
Marko Kaja <marko4449@yahoo.com>
2017-05-18 14:40:54 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(input)
+  if input % 3 == 0 && input % 5 == 0
+    return "FizzBuzz"
+  elsif input % 3 == 0
+    return "Fizz"
+  elsif input % 5 == 0
+    return "Buzz"
+  end
+
+end
