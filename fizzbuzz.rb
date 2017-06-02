1073f43a64724b2e97b66f6495906e8814a073ad
katy be <elvia@gmx.net>
2017-06-02 23:05:51 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && !(int % 5 == 0) # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  elsif int % 5 == 0 && !(int % 3 == 0) # if the number int is divisible by 5
+    "Buzz" # Go Buzz
+  elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 5 and 3
+    "FizzBuzz" 
+  else nil
+  end
+end
