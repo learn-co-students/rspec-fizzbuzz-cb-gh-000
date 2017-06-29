ab18d5c7852d55320c0d2ece9f0ac80217b3246f
salah eddine chaouch <chaouchsalaheddine97@hotmail.fr>
2017-06-29 00:17:53 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(int)
+  if int%3==0 && int%5==0
+    return "FizzBuzz"
+  elsif int%3==0
+    return "Fizz"
+  elsif int%5==0
+    return "Buzz"
+  end
+end
+
+puts fizzbuzz(4)
+puts fizzbuzz(15)
