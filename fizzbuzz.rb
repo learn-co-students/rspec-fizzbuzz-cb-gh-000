140f7471112eacfc59e4ce55c13b394906ffb1ce
Shekh Ataul <ataul.one@gmail.com>
2017-07-01 05:10:26 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(int)
+  if int % 15 == 0
+    "FizzBuzz"
+
+  else
+    if int % 5 == 0
+      "Buzz"
+
+    else
+      if int % 3 == 0
+        "Fizz"
+      end
+
+    end
+
+  end
+end
