5d2596087d751a026df551eb45402ea2b49cd8e9
Matthew Wilson <mwilson1310@gmail.com>
2017-07-07 11:07:18 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  end #end first if statement
+
+  if int % 3 == 0
+    return "Fizz"
+  end #end second if statement
+
+  if int % 5 == 0
+    return "Buzz"
+  end #end third if statement
+
+
+end #end fizzbuzz method
