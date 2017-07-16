a7e3dd5f15a3b9b4660db7f3a4704791b8dafb7f
Hicham Mellouki <silentwell1@gmail.com>
2017-07-16 09:35:33 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(n)
+  if(n % 3 == 0 && n % 5 == 0)
+     "FizzBuzz"
+  elsif (n % 5 == 0)
+     "Buzz"
+  elsif (n % 3 == 0)
+     "Fizz"
+  else
+    return nil
+  end
+end
+
+fizzbuzz(10)
