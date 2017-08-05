79e235557284cfda0652463eed69ba84c6dba4b9
Arnav Choudhury <arnav13@gmail.com>
2017-08-05 18:52:49 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz(num)
+
+
+  if (num % 3 ==0) && (num % 5 !=0)
+
+
+    return "Fizz"
+
+  elsif (num % 5 ==0) && (num % 3 !=0)
+
+    return "Buzz"
+
+  elsif ((num %  5 ==0) && (num % 3 == 0))
+
+    return "FizzBuzz"
+
+  end
+
+end
