1dce92df2d3a849f0b880e0f07f8b4fbb5f857c1
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-16 20:00:53 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,30 @@
 require_relative './spec_helper.rb'
 
-int = gets.strip
-define fizzbuzz()
+define fizzbuzz(int)
+  if (int % 3 == 0)
+    "Fizz"
+elsif (int % 5 == 0)
+   "Buzz"
+ end
+ if (int%3==0 && int%5==0)
+   "Fizzbuzz"
+ else
+   "nil"
+ end
+end
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz()
+    fizz_3 = fizzbuzz(3)
 
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz()
+    fizz_5 = fizzbuzz(5)
 
     expect(fizz_5).to eq("Buzz")
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz()
+    fizz_15 = fizzbuzz(15)
 
     expect(fizz_15).to eq("FizzBuzz")
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,7 @@
 require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
+require_relative '../lib/fizzbuzz.rb'
+
+
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
