4a075bd930dd9f208a4cf9e4c0b99b6f1c04b55b
jonathan hernandez <rockjon.skroc@gmail.com>
2017-11-03 03:50:33 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+
+
+  if (int % 3 == 0  && int % 5 == 0)
+     return "FizzBuzz"
+  elsif int % 3 == 0
+     return "Fizz"
+  else
+     if int % 5 == 0
+       return "Buzz"
+     end
+  end
+
+end
