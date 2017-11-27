8b7951d6f536dd1a1ee4493e5791afe16a54196c
Leandro Küster <mltfrc17@gmail.com>
2017-11-27 19:11:57 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(number)
+  if ( (number % 3 == 0) && (number % 5 == 0) )
+    return "FizzBuzz"
+  elsif (number % 3 == 0)
+    return "Fizz"
+  elsif (number % 5 == 0)
+    return "Buzz"
+  else
+    return nil
+  end
+end
