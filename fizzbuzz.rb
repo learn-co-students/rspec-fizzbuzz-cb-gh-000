a29b3df1973d7188f9daca633abe70ae6e40f8a2
Ahmad Taufiq Hidayat <ataufiq665@gmail.com>
2017-11-29 20:38:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(divisible)
-  if divisible==3
-    "Fizz"
-  elsif divisible==5
-    "Buzz"
-  elsif divisible==15
+  if divisible % 3 == 0 && divisible % 5 == 0
     "FizzBuzz"
+  elsif divisible % 5 == 0
+    "Buzz"
+  elsif divisible % 3 == 0
+    "Fizz"
   else
     nil
   end
