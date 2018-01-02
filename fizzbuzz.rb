b4d0ceb5669abac92597d09322a79d2ded594ec6
Widodo Putro <wd.putro@gmail.com>
2018-01-02 15:34:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz
-
+def fizzbuzz(inp)
+  if (inp % 3 == 0) && (inp % 5 == 0)
+    return "FizzBuzz"
+  elsif inp % 5 == 0
+    return "Buzz"
+  elsif inp % 3 == 0
+    return "Fizz"
+  else
+    return nil
+  end
 end
+
+
+puts fizzbuzz(15)
