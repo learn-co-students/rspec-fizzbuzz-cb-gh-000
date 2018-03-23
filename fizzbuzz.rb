05135bcd5309903927c37920a8d60863de2f5a4b
Harkirat Singh <harkirat.hira@gmail.com>
2018-03-23 15:39:05 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(x)
+  x = x.to_i
+  if (x % 3 == 0) && (x % 5 == 0)
+    return "FizzBuzz"
+  elsif x % 3 == 0
+    return "Fizz"
+  elsif x % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+end
