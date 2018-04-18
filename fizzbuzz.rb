e1ad6c16e11816deaa528915f9f613725e6df1aa
Mason Merril <puckmaster24@yahoo.com>
2018-04-18 01:38:02 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 != 0
+    return "Fizz"
+  elsif num % 5 == 0 && num % 3 != 0
+    return "Buzz"
+  elsif num  % 5 == 0 && num % 3 == 0
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
+
+fizzbuzz(9)
