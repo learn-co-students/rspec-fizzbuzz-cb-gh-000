38201d8f6d5ba4a322248eefb3adeafea33a186e
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-23 07:23:29 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,5 @@
 require_relative './spec_helper.rb'
-require '../fizzbuzz.rb'
+require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
