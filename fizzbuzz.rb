7b9b19d0d5285df55d8160c7dfe4cd3e39e63d92
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-11 13:37:40 -0400

    updated to give more instruction in both the spec file as well as fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,2 @@
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,5 @@
 require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
+# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
