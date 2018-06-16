4fc12b135df01bd3b65977afc30225d29152e108
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-16 01:41:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(user_input)
+  reponse = ""
+  if user_input % 3 == 0
+    reponse += "Fizz"
+  end
+  if user_input % 5 == 0
+    reponse += "Buzz"
+  end
+  if reponse == ""
+    reponse = nil
+  end
+  return reponse
+end
