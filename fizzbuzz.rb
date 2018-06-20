5043e864a404fe411eda81253c22cd32443fdbd8
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-20 13:41:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+if int%3==0
+ "Fizz"
+ elseif(int%5==0)
+ "Buzz"
+ elseif(int%15==0)
+ "FizzBuzz"
+end
+
+end
