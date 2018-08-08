366dafe1e89cf7873248819313bdbb202cf06867
Sergiu Marton <alexander.sergiu@gmail.com>
2018-08-08 06:15:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  result = ""
+  if num % 3 == 0
+    result += "Fizz"
+  end
+  if num % 5 == 0
+    result += "Buzz"
+  end
+  if result == ""
+    nil
+  else
+    result
+  end
+end
