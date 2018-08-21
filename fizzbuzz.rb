7d1cc170722d7ae0364fe8b2e80849f9a45f02eb
Viscount Wathika <vcwathika@gmail.com>
2018-08-21 11:38:58 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  elsif number % 15 == 0
+    return "FizzBuzz"
+  end
+
+end
