08658983983fdccefeae9aadc81e59afb6d3476e
Cihan Akman <cihanozanakman@gmail.com>
2018-11-01 20:16:34 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(value)
+  string = ""
+  if value % 3 == 0
+    string += "Fizz"
+  end
+  if value % 5 == 0
+    string += "Buzz"
+  end
+  if string != ""
+    return string
+  end
+end
