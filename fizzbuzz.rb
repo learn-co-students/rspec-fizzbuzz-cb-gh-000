39d87fb2116c46b13b8655cca0812e85ab9b17e7
Zachary P <z61@zeesixtyone.com>
2018-11-12 03:23:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  string = ""
+  if (int % 3 != 0 && int % 5 != 0)
+    return nil
+  end
+  if (int % 3 == 0)
+    string += "Fizz"
+  end
+  if (int % 5 == 0)
+    string += "Buzz"
+  end
+  return string
+end
