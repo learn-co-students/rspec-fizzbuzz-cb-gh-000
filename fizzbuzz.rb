a606c590ed00b0562960994080f552eff0dc49b4
Aamir Kalimi <habibpramanik1998@gmail.com>
2018-11-18 17:14:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(number)
    "Fizz"
   elsif number % 5 == 0
    "Buzz"
-  elsif (number % 3 == 0 && number % 5 == 0)
+  elsif number % 3 == 0 && number % 5 == 0
   "FizzBuzz"
   end
 end
