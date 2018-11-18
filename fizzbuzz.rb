78712496110db94472054909e61ce810c55c399f
Aamir Kalimi <habibpramanik1998@gmail.com>
2018-11-18 17:12:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0
-    puts "Fizz"
+     "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+     "Buzz"
   elsif (number % 3 == 0 && number % 5 == 0)
-    puts "FizzBuzz"
+     "FizzBuzz"
   end
 end
