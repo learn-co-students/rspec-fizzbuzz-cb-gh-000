5b48b48c608218df0879b5798cda07346164d7ad
Andrew Turner <andrewturner227@gmail.com>
2018-11-20 02:14:50 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+    string = ""
+
+    if int % 3 == 0
+        string += "Fizz"
+    end
+    if int % 5 == 0
+        string += "Buzz"
+    end
+
+    if string == ""
+        string = nil
+    end
+
+    string
+end
