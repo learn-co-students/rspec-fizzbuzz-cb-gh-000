5030172d3ff9dd473f1993285149b95fe4c4f873
Evi Alvanaki <evialvanaki@gmail.com>
2018-12-01 19:23:44 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
+def fizzbuzz(int)
+  if (int % 3 == 0 && int % 5 == 0 ) # if the number int is divisible by 3
+  return  "div3" # Go fizz
+
+elsif int % 5 == 0
+  return "div5"
+
+elsif int % 3 == 0
+return "div3,5"
+
+else
+  return nil
+end
+end
+
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
