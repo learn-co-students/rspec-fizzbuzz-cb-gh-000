37cc1f97b21a481e2de50b79913ed1eb5a30a93f
Harshvardhan Singh <harshvardhan.singh@technonjr.org>
2018-12-07 11:32:11 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if(x%3==0 && x%5==0)
+    return "FizzBuzz"
+  elsif(x%3==0)
+    return "Fizz"
+  elsif(x%5==0)
+    return "Buzz"
+  else
+    return nil
+  end
+end
