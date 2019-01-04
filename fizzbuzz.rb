6df920647aa7f90f6e7ad47bae1d13fe60d1ee69
Bhavesh Choudhary <choudharybhavesh5@gmail.com>
2019-01-04 21:45:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# number = gets.strip
+
+#
+# def fizzbuzz(number)
+#
+#
+# if (number % 3 == 0)
+#   puts "Fizz"
+# elseif (number % 5 == 0)
+#   puts "Buzz"
+# elseif (number % 15 ==0)
+#   puts "fizzbuzz"
+# else (number % 4== 0)
+#   puts "nil"
+# end
+# end
+
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
+end
