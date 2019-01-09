9f385f0d0c4916d7d894771140a45faf288fe4d5
Braden Bowen <sycesx@gmail.com>
2019-01-09 06:00:10 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,16 @@
 require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
+require_relative '../fizzbuzz.rb'
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
 
-    expect(fizz_3).to eq("Fizz")
+    expect(fizz_3).to eq("FizzBuzz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
     fizz_5 = fizzbuzz(5)
 
-    expect(fizz_5).to eq("Buzz")
+    expect(fizz_5).to eq("FizzBuzz")
   end
   it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
     fizz_15 = fizzbuzz(15)
