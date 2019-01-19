e0cf6703513575a94a42b3e8aafa194bef5f4348
Mubeen Ghauri <ghauri.mubeen@gmail.com>
2019-01-19 12:08:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz(args)
+
+  if args % 3 == 0 and args % 5 == 0
+    return "FizzBuzz"
+  elsif args % 3 == 0
+    return "Fizz"
+  elsif args % 5 == 0
+    return "Buzz"
+  end
+end
+
+
+  
