2a8c8c6a5bbeb9354a3667b3ef9a19b375fdcef0
Harrison Grieve <grieve.harrison@gmail.com>
2019-01-30 18:05:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  divBy3 = (n % 3 == 0)
+  divBy5 = (n % 5 == 0)
+
+  if divBy3 and not divBy5
+    "Fizz"
+  elsif divBy5 and not divBy3
+    "Buzz"
+  elsif divBy3 and divBy5
+    "FizzBuzz"
+  else
+    nil
+  end
+
+end
