c9a45cf76cecddedb8c1f492fba66bfb0090290e
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-22 03:17:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# To remember, it's necessary validates all the possibilities
 def fizzbuzz  (number)
   if number % 3 == 0 &&  number % 5 != 0
     "Fizz"
