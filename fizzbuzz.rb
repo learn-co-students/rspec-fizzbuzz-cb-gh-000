4bfdbe01bd692d5b88ed4b2288470a9a4d1ee7a8
Martin Zuleta <mzuleta4@cuc.edu.co>
2019-03-24 03:55:10 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  return "FizzBuzz" if (number.to_i % 3 == 0) && (number.to_i % 5 == 0)
+  return "Fizz" if (number.to_i % 3 == 0)
+  return "Buzz" if (number.to_i % 5 == 0)
+  nil
+end
