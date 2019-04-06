d46962c0301f777c9dc7c89c10f6b93cd9788a54
Fadel Firmansyah <fadel22fauzany@gmail.com>
2019-04-06 02:55:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+
+    if (n % 15 == 0)
+      "FizzBuzz"
+    elsif (n % 3 == 0)
+      "Fizz"
+    elsif (n % 5 == 0)
+      "Buzz"
+    else
+      puts n
+    end
+end
+
+fizzbuzz(20)
