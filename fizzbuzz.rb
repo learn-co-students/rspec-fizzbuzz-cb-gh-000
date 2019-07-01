c2a734be29c66573bbc3a848055fe8f73381462e
Dante Vacirca <dante.vacirca@gmail.com>
2019-07-01 10:09:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
+require 'pry'
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  binding.pry
+
+  if(number % 5 == 0 && number % 3 == 0)
+     "FizzBuzz"
+
+  elsif(number % 3 == 0)
+     "Fizz"
+
+  elsif(number % 5 == 0)
+     "Buzz"
+
+  end
+
+end
