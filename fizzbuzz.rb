dd743b27a266c7ee1c5e0d7e5e560397d443dd7d
Jeremy Bray <godrules148@gmail.com>
2019-07-17 21:14:34 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz (arg)
+    if arg % 3 == 0 && arg % 5 != 0
+        return "Fizz"
+    elsif arg % 3 != 0 && arg % 5 == 0
+        return "Buzz"
+    elsif arg % 3 == 0 && arg % 5 == 0
+        return "FizzBuzz"
+    else
+        return nil
+    end
+
+end
