93664a795c11d059e2cfc2eaa3f3795ddfd4e762
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-19 19:27:29 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(integ)
+  if integ % 3 == 0 && integ % 5 == 0
+    "FizzBuzz"
+  elsif integ % 3 == 0
+    "Fizz"
+  elsif integ % 5 == 0
+    "Buzz"
+  end
+end
