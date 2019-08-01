c4f106a4ce17964329dd011e5209f146674002a2
Joshua Shewmaker <joshuashewmaker@gmail.com>
2019-08-01 01:14:24 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(value)
+  if value % 3 == 0
+    if value % 5 == 0
+       "FizzBuzz"
+    else
+       "Fizz"
+    end
+  elsif value % 5 == 0
+     "Buzz"
+  else
+     nil
+  end
+end
