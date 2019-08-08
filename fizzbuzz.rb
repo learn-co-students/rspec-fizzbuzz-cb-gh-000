6b54c2510c159a048f833b7c17d481d65ca2ab8e
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-08 10:04:18 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  end
+  #if int % 5 == 0
+  #  "Buzz"
+  #end
+#  if int % 3 == 0 && int % 5 == 0
+  #  "FizzBuzz"
+  #end
+  #else
+  #  nil
+#  end
+end
