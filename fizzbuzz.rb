0dac92fedeb349871e1629e3fd26fffc0a49e465
Kevin Hsiao <kevin.hsiao@mail.utoronto.ca>
2019-08-23 01:59:36 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(n)
-  if n % 3 == 0
+  if n % 15 == 0
+    "FizzBuzz"
+  elsif n % 3 == 0
     "Fizz"
   elsif n % 5 == 0
     "Buzz"
-  elsif n % 15 == 0
-    "FizzBuzz"
   else
     nil
+  end
 end
