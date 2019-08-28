9eb49bd21baecf937f9cd7d57c20a5fdcdc3b978
Cankat Saraç <cankat.sarac@stu.bahcesehir.edu.tr>
2019-08-28 06:02:27 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,38 @@
 require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
-describe "fizzbuzz" do
-  it 'returns "Fizz" when the number is divisible by 3' do
-    fizz_3 = fizzbuzz(3)
+require 'rspec'
 
-    expect(fizz_3).to eq("Fizz")
-  end
-  it 'returns "Buzz" when the number is divisible by 5' do
-    fizz_5 = fizzbuzz(5)
-
-    expect(fizz_5).to eq("Buzz")
-  end
-  it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
-    fizz_15 = fizzbuzz(15)
-
-    expect(fizz_15).to eq("FizzBuzz")
-  end
-  it 'returns nil when the number is not divisible by 3 or 5' do
-    fizz_4 = fizzbuzz(4)
-
-    expect(fizz_4).to eq(nil)
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
   end
 end
+
+ RSpec.describe "Fizz Buzz Tests" do
+  it "should return `Fizz` for number divisible by 3" do
+    expect(fizzBuzz(3)).to eq "Fizz"
+  end
+
+   it "should return `Buzz` for number divisible by 5" do
+    expect(fizzBuzz(5)).to eq "Buzz"
+  end
+
+  it "should return `FizzBuzz` for number divisible by 3 and 5" do
+   expect(fizzBuzz(15)).to eq "FizzBuzz"
+ end
+
+ it "should return `FizzBuzz` for number divisible by 3 and 5" do
+   expect(fizzBuzz(90)).to eq "FizzBuzz"
+ end
+
+ it "should return 7 since it is indivisible by 3 and 5" do
+     expect(fizzBuzz(7)).to eq 7
+   end
+ end
+ 
