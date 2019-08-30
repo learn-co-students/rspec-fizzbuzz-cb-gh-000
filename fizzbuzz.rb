35116300646751176bbc5ae9fc2e859d552e0700
Anthony Michaels <ajamesmichaels@gmail.com>
2019-08-30 03:14:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  msg=""
+  if num % 3 == 0
+    msg+="Fizz"
+  end
+  if num % 5 == 0
+    msg+="Buzz"
+  end
+  return msg != "" ? msg : nil
+end
