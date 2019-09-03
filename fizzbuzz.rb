a36099e7c792fd37c4f7407b1350df37a3bf88bf
Lucas Prieels <prieelslucas@gmail.com>
2019-09-03 08:23:38 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  string = ""
+  if (num%3==0)
+    string+="Fizz"
+  end
+  if (num%5==0)
+    string += "Buzz"
+  end
+  if string!=""
+    return string
+  else
+    return nil
+  end
+end
