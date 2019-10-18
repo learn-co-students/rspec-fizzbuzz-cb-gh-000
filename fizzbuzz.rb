29de769dd1b21b48c24475b9532330ec11fc717d
Chidubem Ogbudibe <ogbudibe@mail.utoronto.ca>
2019-10-18 01:30:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,25 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def notfizzbuzz(x)
+  if(x % 5 == 0 && x % 3 == 0)
+    return "FizzBuzz"
+  elsif(x % 3 == 0)
+    return "Fizz"
+  elsif(x % 5 == 0)
+    return "Buzz"
+  end
+end
+
+def fizzbuzz(x)
+  string = ""
+  if(x % 3 == 0)
+    string += "Fizz"
+  end
+  if(x % 5 == 0)
+    string += "Buzz"
+  end
+  if(string != "")
+    return string
+  end
+  return nil
+end
+
+fizzbuzz(15)
