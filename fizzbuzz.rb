df5fb7a820b0555c0a69c7d02aa212f36eafe4b8
Fouad Ahsayni <f.ahsayni@pm.me>
2020-01-22 19:34:06 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(i)
+
+  if i % 3 == 0 && i % 5 == 0
+    return "FizzBuzz"
+  elsif i % 5 == 0
+    return "Buzz"
+  elsif i % 3 == 0
+    return "Fizz"
+  else
+    return nil
+  end
+
+end
