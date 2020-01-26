b17796aaa454a45174de7b64bd188a287f79e387
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-01-26 21:29:17 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+
+def fizzbuzz(number)
+    if number%5==0 && number%3==0
+      return "FizzBuzz"
+      
+    elsif number%3==0
+    return "Fizz"
+
+    elsif number %5==0
+      return "Buzz"
+    end
+    nil
+end
