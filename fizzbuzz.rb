120fa5473df0433cffec3a55b2c59de8a4cf5ba4
Inas Sati <21830264@students.liu.edu.lb>
2020-01-29 10:08:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(nb)
+  if (nb % 3) == 0
+    return "Fizz"
+  elsif (nb % 5) == 0
+    return "Buzz"
+  elsif (nb % 15) == 0
+    return"FizzBuzz"
+  else
+    return nil
+end
