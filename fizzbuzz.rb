e79f9f4b154444c20333d27bfe9c9f1cffc50c80
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 05:36:56 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 ==0
+    "Fizz"
+  else
+    nil
+  end
+end
+puts fizzbuzz(5)
