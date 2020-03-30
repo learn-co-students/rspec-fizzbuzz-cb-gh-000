5c3582d0625bb0adaa7974a6a000c82208733ed0
Shubham Anand <shubham.rubal.31@gmail.com>
2020-03-30 18:59:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num%15 == 0
+    return "FizzBuzz"
+  elsif num%5 == 0
+    return "Buzz"
+  elsif num%3 == 0
+    return "Fizz"
+  end
+end
