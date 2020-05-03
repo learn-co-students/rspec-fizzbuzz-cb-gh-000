9ff7d113f9c7fc02bb563fc9aad418aece1d1b4c
Danis Abadia <deanis89@hotmail.com>
2020-05-03 13:37:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(numero)
+  orden = ""
+  if numero.to_i % 3 == 0
+    orden += "Fizz"
+  end
+  if numero.to_i % 5 == 0
+    orden += "Buzz"
+  end
+  if orden == ""
+    orden = nil
+  else
+    numero = orden
+  end
+end
