58c1bab3d28e771b1bfe2d48f5268d87567b0749
Atul Tiwari <atul.tiwari@krishnacollege.ac.in>
2020-06-01 11:22:28 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+ if((number%3==0) && (number%5==0))
+    return "FizzBuzz"
+ elsif(number%3 == 0)
+    "Fizz"
+  elsif(number%5 == 0)
+    "Buzz"
+  else
+    nil
+  end
+end
+
+fizzbuzz(5)
