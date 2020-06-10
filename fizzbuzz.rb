69ce25e9ed0efd589a856e46acd5c4c7ed32d0d8
Rutvik Joshi <rsuperiorj77@gmail.com>
2020-06-10 11:46:02 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(n)
-  if n == 3
-    puts "Fizz"
-  elsif n==5
-    puts "Buzz"
-  elsif n == 15
-    puts "FizzBuzz"
-  else
-    return nil
-end
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+      "FizzBuzz"
+    elsif int % 3 == 0
+      "Fizz"
+    elsif int % 5 == 0
+      "Buzz"
+    end
+  end
