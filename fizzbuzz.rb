337257b8ea4a88b745b16595e6b1f5f20507f033
Adarsh Raj Anand <adarsh1607077@nitp.ac.in>
2020-07-18 15:28:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    nil
+  end
+end
